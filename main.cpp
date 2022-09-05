ad4df74dd78c875e7e040a758b4e88d40a1509b4
Anar95 <anarabbas458@gmail.com>
2022-09-06 00:48:40 +0300

    c++

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  C++ Deyışken deyerın deysme
+//
+//  Created by Anar Abbas on 4.09.2022.
+//
+
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+    int x = 20;
+    int girilen;
+    cout << "Bır sayı gırın"<< endl;
+    cin>>girilen;
+    if(girilen>20){
+        cout <<" Gırdıyınız regem boyukdur n";
+        return main();
+    } else if (girilen<20){
+    cout <<" Gırdıyınız regem kıcıkdır n";
+    return main();
+} else{
+    cout <<" TEBRIKLER TAPDINIZ! n";
+}
+}
+
+}
+
+    
+
